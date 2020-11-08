984f703cbb81c8f66a3dbb3efe99fdf1f39ffa04
Simon Harms <simonharms3@gmail.com>
2020-11-07 20:19:58 -0500

    Finally tested my code, fixed a lot of bugs. All functions now assume we are working in two dimensions

--- simulation_calculations.py
+++ simulation_calculations.py
@@ -3,6 +3,8 @@ import numpy
 permittivity = 8.9e-12
 permeability = 1.3e-6
 
+
+
 class Field:
     def get_electric_field(self,pos):
         pass
@@ -18,23 +20,21 @@ class RectField(Field):
         self.magnetic = mag
 
     def inside(self,pos):
-        for i in range(pos):
-            dist = pos[i] - corner_pos[i]
-            if dist < 0 or dist < dimension[i]:
-                return False
+        if pos[0] - self.position[0] < 0 or pos[0] - self.position[0] < self.dimensions[0] or pos[1] - self.position[1] < 0 or pos[1] - self.position[1] < self.dimensions[1]:
+            return False
         return True
 
     def get_electric_field(self,pos):
-        if inside(self,pos) and not magnetic:
-            return value
+        if self.inside(pos) and not self.magnetic:
+            return self.value
         else:
-            return [0 for n in value]
+            return [0, 0]
 
     def get_magnetic_field(self,pos):
-        if inside(self,pos) and magnetic:
-            return value
+        if self.inside(pos) and self.magnetic:
+            return self.value
         else:
-            return [0 for n in value]
+            return [0, 0, 0]
 
 class PointCharge(Field):
     def __init__(self,pos,cha):
@@ -42,10 +42,10 @@ class PointCharge(Field):
         self.charge = cha
 
     def get_electric_field(self,pos):
-        return charge / (4 * numpy.pi * permittivity) * (pos - self.position) / n(numpy.linalg.norm(pos - self.position)**3)
+        return self.charge / (4 * numpy.pi * permittivity) * numpy.subtract(pos, self.position) / numpy.linalg.norm(numpy.subtract(pos, self.position))**3
 
     def get_magnetic_field(self,pos):
-        return [0 for x in pos]
+        return [0, 0, 0]
 
 class WireZ(Field):
     def __init__(self,pos,cur):
@@ -53,10 +53,10 @@ class WireZ(Field):
         self.current = cur
 
     def get_electric_field(self, pos):
-        return [0 for x in pos]
+        return [0, 0]
 
     def get_magnetic_field(self,pos):
-        return numpy.cross_product([0,0,self.current],pos - self.position) * permeability / (2 * numpy.pi * numpy.linalg.norm(pos - self.position,2))
+        return numpy.cross([0,0,self.current],numpy.subtract(pos, self.position)) * permeability / (2 * numpy.pi * numpy.linalg.norm(numpy.subtract(pos, self.position),2))
 
 class InfiniteWire(Field):
     def __init__(self,pos,dire,cur):
@@ -65,18 +65,18 @@ class InfiniteWire(Field):
         self.current = cur
 
     def get_electric_field(self,pos):
-        return [0 for x in pos]
+        return [0, 0]
 
     def get_magnetic_field(self,pos):
-        return current * permeability * numpy.cross_product(self.direction,pos - self.position) / (2 * numpy.pi * numpy.linalg.norm(numpy.cross_product(self.direction,pos - self.position),2))
+        return self.current * permeability * numpy.cross(self.direction,numpy.subtract(pos, self.position)) / (2 * numpy.pi * numpy.cross(self.direction,numpy.subtract(pos, self.position)**2))
 
 def force(charge,pos,vel,fields):
-    elec = [0 for x in pos]
-    mag = [0 for x in pos]
+    elec = [0, 0]
+    mag = [0, 0, 0] #Magnetic fields are three dimensions to account for the z axis
     for field in fields:
-        elec += field.get_electric_field
-        mag += field.get_magnetic_field
-    return charge * (elec + numpy.cross_product(vel,mag))
+        elec = numpy.add(elec, field.get_electric_field(pos))
+        mag = numpy.add(mag, field.get_magnetic_field(pos))
+    return charge * numpy.add(elec, numpy.cross(vel,mag)[0:-1])
 
 class Charge:
     def __init__(self,pos,vel,cha,mas):
@@ -85,14 +85,14 @@ class Charge:
         self.charge = cha
         self.mass = mas
 
-def time_step(charges,dt):
+def time_step(charges,fields,dt):
     for charge in charges:
         #We use a 4th order runge-kutta method to calculate the change in velocity
         #we can do this because our force function gives us a definite function for acceleration
-        k1 = force(charge.charge, charge.position, charge.velocity) / charge.mass
-        k2 = force(charge.charge, charge.position + k1 / 2, charge.velocity) / charge.mass
-        k3 = force(charge.charge, charge.position + k2 / 2, charge.velocity) / charge.mass
-        k4 = force(charge.charge, charge.position + k3 / 2, charge.velocity) / charge.mass
+        k1 = force(charge.charge, charge.position, charge.velocity, fields) / charge.mass
+        k2 = force(charge.charge, charge.position + k1 / 2, charge.velocity, fields) / charge.mass
+        k3 = force(charge.charge, charge.position + k2 / 2, charge.velocity, fields) / charge.mass
+        k4 = force(charge.charge, charge.position + k3 / 2, charge.velocity, fields) / charge.mass
         charge.velocity += 1/6 * dt * (k1 + 2 * k2 + 2 * k3 + k4)
         #Since we're determining velocity numerically we can't use the same method here so we settle for the less accurate Euler approximation
         charge.position += charge.velocity * dt
